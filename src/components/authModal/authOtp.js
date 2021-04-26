c7e1aa3eb04193ef2267cbd20fd58817c59b0ed3
Kevin Wang <kw1017@gmail.com>
2021-04-25 18:08:56 -0700

    added vali num keydown function

--- src/components/authModal/authOtp.js
+++ src/components/authModal/authOtp.js
@@ -4,18 +4,35 @@ import './index.css'
 
 
 function AuthOtp() {
-
-    const [valiNum, setValiNum] = useState([]);
+    // const [valiNum, setValiNum] = useState([]);
 
     console.log('render')
-    console.log(valiNum)
+    // console.log(valiNum)
+    // console.log(valiNum.length)
 
-    function displayNum(e) {
-       setValiNum(valiNum => [...valiNum, e.key])
-    }
 
     // Keydown event listener
     useEffect(() => {
+        let valiNum = [];
+        let numField = document.querySelector('.otp-validation-num').childNodes;
+
+        function displayNum(e) {
+            // Check if its 0-9 and less then 4 digits - push the input in array
+            if(e.keyCode >= 48 && e.keyCode <= 57 && valiNum.length <= 3) {
+                // setValiNum(valiNum => [...valiNum, e.key])
+                valiNum.push(e.key)
+                valiNum.forEach((num, index) => {
+                    numField[index].classList.add('fill');
+                    numField[index].innerText = num;
+                })
+            } else if(e.keyCode === 8) {
+                valiNum.pop();
+                numField[valiNum.length].classList.remove('fill');
+                numField[valiNum.length].innerText = '';
+                
+            }
+        }
+
         
         window.addEventListener('keydown', displayNum);
 
@@ -34,10 +51,10 @@ function AuthOtp() {
                     <p>(+886) 92200000</p>
                 </div>
                 <div className="otp-validation-num">
-                    <span className="validation-num">1</span>
-                    <span className="validation-num">2</span>
-                    <span className="validation-num">3</span>
-                    <span className="validation-num">4</span>
+                    <span className="validation-num"></span>
+                    <span className="validation-num"></span>
+                    <span className="validation-num"></span>
+                    <span className="validation-num"></span>
                 </div>
             </div>
         </>
