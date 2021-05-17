62f4028f7d40889ed14fc18beac3ec2aca3a382d
Kevin Wang <kw1017@gmail.com>
2021-05-16 20:52:57 -0700

    next btn func

--- src/components/modalSignUp/index.js
+++ src/components/modalSignUp/index.js
@@ -1,16 +1,21 @@
 import './index.css'
+import { ContextProvider } from '../modalAuth'
+import { useContext } from 'react'
 import InputField from '../inputField'
 import BtnNext from '../btnNext'
 import BtnGoogle from '../btnGoogle'
 import BtnFacebook from '../btnFacebook'
 
 
-function ModalSignUp({ handleNext, login }) {
+function ModalSignUp({ login }) {
+
+    const contextObject = useContext(ContextProvider)
+    const nextBtnFunc = contextObject.nextBtnFunc
     
     return (
         <div className="inner-modal-content">
             <InputField className="signup-modal-phone-email" type="text" placeholder="電話號碼/ Email" />
-            <BtnNext onClick={handleNext} className="btn-next-signup"/>
+            <BtnNext onClick={nextBtnFunc} className="btn-next-signup"/>
             <h2 className="horizontal-line"><span className="horizontal-line-text">或</span></h2>
             <div className="modal-social-btns">
                 <BtnGoogle />
