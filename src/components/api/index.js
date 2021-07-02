1062d9b5666d1823f4dacce461d52650675af109
Kevin Wang <kw1017@gmail.com>
2021-07-01 23:53:32 -0700

    api

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import './index.css'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+})
 
 function Api() {
 
+    function fetchUsers() {
+        axiosInstance.get('/todos')
+        .then(res => {console.log(res)})
+    }
+
+    // useEffect(() => {
+    //     fetchUsers()
+    // }, [])
 
     return (
-        <div className="user-container">
+        <div className="api-user-container">
 
-            <h1 className="user-title">USER LIST</h1>
+            <h1 className="api-user-title">USERS LIST</h1>
 
         </div>
     )
